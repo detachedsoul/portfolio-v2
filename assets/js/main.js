f82e4994351376e6bcbd1638a33c0377db80825a
detachedsoul <vindicatedwisdom@gmail.com>
2021-04-24 01:49:37 +0100

    Rough design with some few bugs

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+let navLinks        =   document.querySelectorAll('.nav-link');
+let navContainer    =   document.querySelector('.nav-content');
+let navToggle       =   document.querySelector('.nav-toggle-btn');
+let toTop           =   document.querySelector('.to-top');
+
+function removeActiveClass() {
+    navLinks.forEach(elem => elem.classList.remove('active'));
+    this.classList.add('active');
+    navContainer.classList.remove('nav-content-show');
+    navToggle.classList.remove('nav-toggle-close');
+}
+
+function toggleNavControlButton() {
+    navContainer.classList.toggle('nav-content-show');
+    navToggle.classList.toggle('nav-toggle-close');
+}
+
+navToggle.addEventListener('click', toggleNavControlButton);
+navLinks.forEach(elem => {
+    elem.addEventListener('click', removeActiveClass);
+});
+
+toTop.addEventListener('click', () => {
+    setTimeout(() => {
+        scrollTo({
+            'top': 0,
+            'left': 0,
+            'behavior': 'smooth'
+        })
+    }, 600)
+});
